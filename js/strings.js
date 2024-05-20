2e5b7b586e721effb1248d988163d674bcdb31ff
Liliia <LiliiaPav@gmail.com>
2024-05-20 10:44:52 -0700

    String started

---
+++ js/strings.js
@@ -0,0 +1,32 @@
+//STEP 1
+// let userName=prompt("Enter your name please");
+// alert(userName.length)
+
+//STEP 2
+// let userName=prompt("Enter your name please");
+// let number=parseInt(prompt(`Enter a number between 0 and ${userName.length-1}`));
+// if (number>=0 && number<=userName.length-1){
+//      alert(userName.charAt(number));
+// }
+// else {
+//      alert("Wrong number")
+// }
+
+//STEP 3
+let firstName=prompt("Enter your first name please");
+let lasttName=prompt("Enter your last name please");
+alert('Your name is:'.concat(' ', firstName, ' ', lasttName));
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
