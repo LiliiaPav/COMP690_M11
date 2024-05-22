7d6bb39b2f5d16ab57bcb5fbc1f9e4154fae6df4
Liliia <LiliiaPav@gmail.com>
2024-05-22 10:02:42 -0700

    Math ended

--- js/math.js
+++ js/math.js
@@ -1,9 +1,24 @@
 //STEP 1
+// let number=parseInt(prompt(`Enter a number`));
+// console.log(Math.abs(number))
 
 //STEP 2
+// let number=parseFloat(prompt(`Enter a number`));
+// console.log(Math.ceil(number))
 
 //STEP 3
+// let number=parseFloat(prompt(`Enter a number`));
+// console.log(Math.floor(number))
 
 //STEP 4
+// let numbers=prompt(`Enter a 5 numbers, separated by coma(ex. 1,2,3,4,5) `);
+// let result= numbers.split(',')
+// let temp=[];
+// for (let i of result){
+//     temp.push(parseInt(i));
+// }
+// console.log(`Minimun is ${Math.min(...temp)}, Maximum is ${Math.max(...temp)}`);
 
 //STEP 5
+let number=parseFloat(prompt(`Enter a number`));
+console.log(Math. sqrt(number))
